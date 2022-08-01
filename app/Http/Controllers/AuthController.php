172d3e67a81de872b5808a87ad0f960612b2e0f2
esakimedia <apachecordovax@gmail.com>
2022-08-02 00:09:53 +0200

    replacement of name by pseudo

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,7 +12,7 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         $data = $request->validate([
-            'name' => 'required|string',
+            'pseudo' => 'required|string',
             'email' => 'required|email|string|unique:users,email',
             'password' => [
                 'required',
@@ -22,7 +22,7 @@ class AuthController extends Controller
         ]);
 
         $user = User::create([
-            'name' => $data['name'],
+            'pseudo' => $data['pseudo'],
             'email' => $data['email'],
             'password' => bcrypt($data['password'])
         ]);
