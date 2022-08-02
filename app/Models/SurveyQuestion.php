a40c355a16e7b88754ef6a43e3d5d879e8ca5eb7
esakimedia <apachecordovax@gmail.com>
2022-08-02 20:48:35 +0200

    DashboardController and dashboard route

--- app/Models/SurveyQuestion.php
+++ app/Models/SurveyQuestion.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Survey;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class SurveyQuestion extends Model
 {
